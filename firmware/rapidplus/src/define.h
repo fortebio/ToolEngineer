1322f8786e0f0bc3a0ab87c039336daebbe3a00e
Kane Nguyen <work.kanenguyen@gmail.com>
2025-12-08 16:13:06 +0700

    update 2.3.5

--- firmware/rapidplus/src/define.h
+++ firmware/rapidplus/src/define.h
@@ -284,8 +284,8 @@ struct parastructure
 
 // sensor setting
 #define ChannelEnableSet ChannelEnableSetBoth
-// #define ALSITSet ALSITSet100
-#define ALSITSet ALSITSet200
+#define ALSITSet ALSITSet100
+// #define ALSITSet ALSITSet200
 #define GAINSet GAINSetDouble
 #define DGSet DGSetNormal
 #define SENSSet SENSSetHigh
